18ae1e435bb50d04ab913559e26099e309bc8458
Mike <mpullen9@gmail.com>
2021-11-26 11:24:53 +0100

    Add disconnect cmd

--- backend/react/src/components/Management.js
+++ backend/react/src/components/Management.js
@@ -1,8 +1,8 @@
 import '../App.css';
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
 import ReactConsole from '@webscopeio/react-console';
-import { config } from '../Constants'
+import { config } from '../Constants';
+import { getMainSocket, disconnectMain } from './MainSocketSingleton';
 var ENDPOINT = config.url.API_URL;
 
 const Management = () => {
@@ -19,35 +19,37 @@ const Management = () => {
   const [matrixMode, setMatrixMode] = useState(false);
   const [diffs, setDiffs] = useState([]);
 
-const connect = () => {
-  const socket = socketIOClient(ENDPOINT);
-  socket.on("FromAPI", data => {
 
-    const timeData = data.split(";")[0]
-    const activeConData = data.split(";")[1]
 
-    //active connections
-    setActiveCons(activeConData)
+  const connect = () => {
+    const socket = getMainSocket(ENDPOINT)
+    socket.on("FromAPI", data => {
 
-    const currTime = new Date().getTime();
-    const remoteMs = Date.parse(timeData);
+      const timeData = data.split(";")[0]
+      const activeConData = data.split(";")[1]
 
-    //calculate differents between remote and local
-    const diff = currTime - remoteMs;
-    if(diff < 0){
-      return;
-    }
-    setDiffTime(diff);
-    if(diffs > 100){
-      (diffs.splice(1, 100))
-    }
-    if(diff>=0){
-      setDiffs( arr => [...arr, diff]);
-    }
-    setResponse(remoteMs + "");
-    setLocalTime(currTime + "");
-  });
-}
+      //active connections
+      setActiveCons(activeConData)
+
+      const currTime = new Date().getTime();
+      const remoteMs = Date.parse(timeData);
+
+      //calculate differents between remote and local
+      const diff = currTime - remoteMs;
+      if (diff < 0) {
+        return;
+      }
+      setDiffTime(diff);
+      if (diffs > 100) {
+        (diffs.splice(1, 100))
+      }
+      if (diff >= 0) {
+        setDiffs(arr => [...arr, diff]);
+      }
+      setResponse(remoteMs + "");
+      setLocalTime(currTime + "");
+    });
+  }
 
   useEffect(() => {
     connect();
@@ -55,7 +57,7 @@ const connect = () => {
 
   const calcMatrix = () => {
     let resultMatrix;
-    matrix.forEach((part, index,array) => {
+    matrix.forEach((part, index, array) => {
       var colLength = Math.floor(Math.random() * 20);
       var col = Array.apply(null, Array(colLength))
       col.forEach((colPart, colIndex, colArray) => {
@@ -63,8 +65,8 @@ const connect = () => {
         var char = charSet[randRep];
         colArray[colIndex] = char;
       })
-    array[index] = col;
-    resultMatrix = array;
+      array[index] = col;
+      resultMatrix = array;
     })
     return resultMatrix;
   }
@@ -75,20 +77,20 @@ const connect = () => {
     return avg;
   }
 
-  const healthCheck = ()  => {
+  const healthCheck = () => {
     const currTime = new Date().getTime();
     var state = "UNKNOWN"
     var msg = ""
-    if(Number.isNaN(+localTime)){
-      state="CRITICAL"
+    if (Number.isNaN(+localTime)) {
+      state = "CRITICAL"
       msg = " - NO PREV TIME FOUND"
       setHealth(state);
       return state + msg;
     }
-    if(parseInt(localTime) - 3000 > currTime){
+    if (parseInt(localTime) - 3000 > currTime) {
       state = "CRITICAL"
-    }else{
-      state ="HEALTHY"
+    } else {
+      state = "HEALTHY"
     }
     setHealth(state);
     return state + msg;
@@ -122,116 +124,125 @@ const connect = () => {
         })
       }
     },
-  health: {
-    description: 'Check health of backend server',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          setHistory([...history, "health"])
-          resolve(healthCheck())
-        }, 1000)
-      })
-    }
-  },
-  rickroll: {
-    description: '¯\\_(ツ)_/¯.',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          setHistory([...history, "rickroll"])
-          window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-          resolve("hihihihi");
-        }, 1000)
-      })
-    }
-  },
-  help: {
-    description: 'List hints & commands',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          setHistory([...history, "help"])
-         var res = Object.entries(customCommands).map((key,value) => key[0] + " - " + key[1].description + "\n")
-         resolve(res.join(""));
-        }, 1000)
-      })
-    }
-  },
-  matrix: {
-    description: 'Enter the matrix',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-        setMatrixMode(true)
-        setTimeout(() => {
-          setHistory([...history, "matrix"])
-          setMatrixMode(false)
-          resolve("Hello Neo")
-        }, 15000)
-      })
-    }
-  },
-  forceconnect: {
-    description: 'Force to create new connection',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
+    health: {
+      description: 'Check health of backend server',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            setHistory([...history, "health"])
+            resolve(healthCheck())
+          }, 1000)
+        })
+      }
+    },
+    rickroll: {
+      description: '¯\\_(ツ)_/¯.',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            setHistory([...history, "rickroll"])
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            resolve("hihihihi");
+          }, 1000)
+        })
+      }
+    },
+    help: {
+      description: 'List hints & commands',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            setHistory([...history, "help"])
+            var res = Object.entries(customCommands).map((key, value) => key[0] + " - " + key[1].description + "\n")
+            resolve(res.join(""));
+          }, 1000)
+        })
+      }
+    },
+    matrix: {
+      description: 'Enter the matrix',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          setMatrixMode(true)
+          setTimeout(() => {
+            setHistory([...history, "matrix"])
+            setMatrixMode(false)
+            resolve("Hello Neo")
+          }, 15000)
+        })
+      }
+    },
+    forceconnect: {
+      description: 'Force to create new connection',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
           connect();
           resolve();
-      })
+        })
+      }
+    },
+    setport: {
+      description: 'Set New Port',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          var port = args[0]
+          ENDPOINT = "http://localhost:" + port;
+          resolve("Updated port to " + port);
+        })
+      }
+    },
+    setendpoint: {
+      description: 'Set New Endpoint (https://xxx:xx)',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          ENDPOINT = args[0]
+          resolve("Updated endpoint to " + ENDPOINT);
+        })
+      }
+    },
+    endpoint: {
+      description: 'Display Endpoint',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          resolve("Current " + ENDPOINT);
+        })
+      }
+    },
+    disconnectmain: {
+      description: 'Disconnects main socket',
+      fn: (...args) => {
+        return new Promise((resolve, reject) => {
+          disconnectMain();
+          resolve("Disconnected main socket, use forceconnect to reconned ");
+        })
+      }
     }
-  },
-  setport: {
-    description: 'Set New Port',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-         var port = args[0]
-         ENDPOINT = "http://localhost:"+port;
-         resolve("Updated port to " + port);
-      })
-    }
-  },
-  setendpoint: {
-    description: 'Set New Endpoint (https://xxx:xx)',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-         ENDPOINT = args[0] 
-         resolve("Updated endpoint to " + ENDPOINT);
-      })
-    }
-  },
-  endpoint: {
-    description: 'read Endpoint',
-    fn: (...args) => {
-      return new Promise((resolve, reject) => {
-         resolve("Current " + ENDPOINT);
-      })
-    }
-  }
   }
 
   return (
     <main className="area" >
       {matrixMode &&
-      <div unselectable="on"  class="inbackground unselectable">
-        <div className="mover">
-          <div className="matrix-container">
-          {calcMatrix().map((col, i) => {
-            return (<div className="matrix-col">
-              {col.map((char,i) => {
-              return (<p className="m-letter">{char}</p>)
-              })
-            })
+        <div unselectable="on" class="inbackground unselectable">
+          <div className="mover">
+            <div className="matrix-container">
+              {calcMatrix().map((col, i) => {
+                return (<div className="matrix-col">
+                  {col.map((char, i) => {
+                    return (<p className="m-letter">{char}</p>)
+                  })
+                  })
+                </div>
+                )
+              })}
             </div>
-            )
-          })}
           </div>
         </div>
-      </div>
-       }
+      }
       <h1>PREN 11</h1>
       <div className="live-data" style={{ borderBottom: "3px solid green" }} >
         <h3>Analytics (HEALTH CHECK: <span className={`health-result ${health.toLowerCase()}`}>{health}</span>)</h3>
-        <div style={{ display: 'flex', flexDirection: 'row', width: '100v'}}>
-          <div style={{flexGrow:1}}>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100v' }}>
+          <div style={{ flexGrow: 1 }}>
             <p>
               Time on Remote &emsp; {response}
             </p>
@@ -241,12 +252,12 @@ const connect = () => {
             <h2>Difference: <span>{diffTime}ms</span> // Average: {calcAverage(diffs)}ms
             </h2>
           </div>
-          <div style={{flexGrow:1, float:'right', textAlign:'end', marginRight:'2em'}}>
+          <div style={{ flexGrow: 1, float: 'right', textAlign: 'end', marginRight: '2em' }}>
             <p>active connections: {activeCons}</p>
           </div>
         </div>
       </div>
-      <div className="console" style={{ height: "50%%", flexGrow: "1", display: "flex", flexDirection: "column" , zIndex:100, background:"none"}}>
+      <div className="console" style={{ height: "50%%", flexGrow: "1", display: "flex", flexDirection: "column", zIndex: 100, background: "none" }}>
         <ReactConsole
           wrapperStyle={{ flexGrow: "1" }}
           autoFocus
