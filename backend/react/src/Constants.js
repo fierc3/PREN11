deef7ccaa5d3f2478c0f24dc5286d6e527437acd
Mike <mpullen9@gmail.com>
2021-12-03 11:05:11 +0100

    Update constants

--- backend/react/src/Constants.js
+++ backend/react/src/Constants.js
@@ -1,7 +1,7 @@
 // Constants.js
 const prod = {
     url: {
-     API_URL: 'https://tactile-rigging-333212.oa.r.appspot.com:3001'
+     API_URL: 'https://tactile-rigging-333212.oa.r.appspot.com'
     }
    };
    const dev = {
