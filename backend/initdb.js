cb363ac62b8bdff95be60f6a4ccfc3219b611e7d
Mike <mpullen9@gmail.com>
2022-03-09 22:39:53 +0100

    Add basic db setup

---
+++ backend/initdb.js
@@ -0,0 +1,25 @@
+const sqlite3 = require('sqlite3').verbose();
+
+console.log('Started init of sqllite database')
+// open a database connection
+let db = new sqlite3.Database('./techradar.db');
+
+
+// creating event table
+let createEventsTable=`
+CREATE TABLE event (
+    event_id INTEGER PRIMARY KEY,
+    run INTEGER NOT NULL,
+    datetime TEXT NOT NULL,
+    event_type INTEGER NOT NULL,
+    event_value TEXT NOT NULL
+);
+`;
+
+db.exec(createEventsTable, (err) => {
+    if(err){
+        console.warn("Error while creating user table", err)
+    }else{
+        console.log("Successfully created db");
+    }
+})
