95cc5f209c4500061ac7a096102d47c7b6ce1752
Mike <mpullen9@gmail.com>
2022-04-29 10:35:06 +0200

    Upgrade to .net 6

---
+++ robot/PREN11-PI.backup/PiController/PlantIdApiHandler.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+// using System.DrawingCore.Bitmap; 
+public class PlantIdApiHandler
+{
+    public static async Task<string> SendToPlantIdAsync(byte[] imageArray)
+    {
+
+        string url = "https://api.plant.id/v2/identify";
+        string api_key = "LYsccirRB9jadHsq8e2j7ddsDOLihh0LiITrGgkKDtu0oa8uLC";
+
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(url);
+
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        client.DefaultRequestHeaders.Add("Api-Key", api_key);
+
+        List<string> images = new List<string>();
+        //byte[] imageArray = System.IO.File.ReadAllBytes("C:/Users/stefa/OneDrive/Pictures/plant.png");
+        string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+        images.Add(base64ImageRepresentation);
+
+        JObject payload = new JObject(
+            new JProperty("images",
+                new JArray(
+                    from i in images
+                    select new JValue(i)
+                )
+            ),
+            new JProperty("plant_details", new JArray(new JValue("common_names")))
+        );
+
+        var stringContent = new StringContent(JsonConvert.SerializeObject(payload, Formatting.Indented));
+        var response = await client.PostAsync(url, stringContent);
+        var result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(response.ToString());
+        Console.WriteLine(result);
+        dynamic resultJson = JObject.Parse(result);
+
+        string finalPlant = "";
+        float finalProbability = 0;
+        foreach (var suggestion in resultJson.suggestions)
+        {
+            string plant = "unknown";
+            if (suggestion.plant_details.common_names.Type == JTokenType.Array)
+            {
+                plant = suggestion.plant_details.common_names[0];
+            }
+            else
+            {
+                plant = suggestion.plant_details.common_names;
+            }
+            
+            float probability = suggestion.probability;
+
+
+            Console.WriteLine("Plant name: " + plant);
+            Console.WriteLine("Probability: " + probability);
+            if(probability > finalProbability)
+            {
+                finalPlant = plant;
+                finalProbability = probability;
+            }
+        }
+        Console.WriteLine("FINAL RESULT FOR PLANT IS: " + finalPlant + " | " + finalProbability);
+        return finalPlant;
+    }
+
+    public static string SendToPlantId(byte[] imageArray)
+    {
+        var asyncResult = SendToPlantIdAsync(imageArray);
+        return asyncResult.GetAwaiter().GetResult();
+    }
+}
---
+++ robot/PREN11-PI.backup/PiController/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using QrCodeDetection;
+
+namespace PiController
+{
+
+    class Program
+    {
+        static int selection = 1; //1 = webcam, 2 = PI camera
+        static int visualize = 1; //1 = visualize, 2 = dont visualize
+        static int mode = 1; //1 = offline, 2 = online
+
+        private static void loadFlags(string[] args)
+        {
+            if (args.Length > 0)
+                int.TryParse(args[0], out selection);
+            if (args.Length > 1)
+                int.TryParse(args[1], out visualize);
+            if (args.Length > 2)
+                int.TryParse(args[2], out mode);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("++++++ PREN11 PiController is starting ++++++");
+            Console.WriteLine("Using args: " + String.Join(", ", args));
+
+            loadFlags(args);
+            var localGuid = Guid.NewGuid();
+            Console.WriteLine("Loaded args, setting up run: " + localGuid.ToString());
+            Detector detector = null;
+            ServerCommunicator serverCommunicator = null;
+            try
+            {
+                detector = new QrCodeDetection.Detector();
+                //1. Try and setup connection with all components (server connection, camera test...)
+                try
+                {
+                    serverCommunicator = new ServerCommunicator("https://tactile-rigging-333212.oa.r.appspot.com");
+                    detector.Init(visualize == 1);
+                    Console.WriteLine("Setting up Server Communicator");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Couldn't setup run {localGuid} because of an error: {ex.Message}");
+                    Console.Error.WriteLine(ex.StackTrace.ToString());
+                    throw ex; // throw so it cleans up run correctly
+                }
+                Boolean end = false;
+                //2. Start Run
+                serverCommunicator.SendStart();
+                while (end == false)
+                {
+                    var latestValue = detector.DetectUniqueCode();
+                    // process value
+                    end = (latestValue.text.Equals("end"));
+                    if(end == false && latestValue.text.Contains("plant"))
+                    {
+                        //send to plantId
+                        if (mode == 2) //Online Mode check
+                        {
+                            Console.WriteLine("Running in Online Mode, preparing to send image to plantId");
+                            var imageBytes = latestValue.image.ToBytes();
+                            var plantIdResult = PlantIdApiHandler.SendToPlantId(imageBytes);
+                            // Send Result to Webserver via socket.io
+                            Console.WriteLine("Sending found plant via socket: " + plantIdResult);
+                            serverCommunicator.SendPlant(plantIdResult);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Run {localGuid} ended because of an error: {ex.Message}");
+                Console.Error.WriteLine(ex.StackTrace.ToString());
+                //we could display these errors somewhere
+            }
+            finally
+            {
+                Console.WriteLine("Ending Run");
+                if(detector != null)
+                {
+                    detector.Release();
+                }
+                if(serverCommunicator != null)
+                {
+                    serverCommunicator.SendEnd();
+                }
+                Console.ReadKey(); //for debug purpose
+                //end run
+            }
+
+
+
+        }
+    }
+}
