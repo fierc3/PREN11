72f33987cf17affc098b367935deac06a0d3c904
Mike <mpullen9@gmail.com>
2022-05-06 18:12:04 +0200

    Add 50ms to work time over shutterspeed

--- robot/PREN11-PI/PiController/CameraPiModule.cs
+++ robot/PREN11-PI/PiController/CameraPiModule.cs
@@ -89,7 +89,7 @@ namespace Camera
                 cam.Camera.PreviewPort.ConnectTo(nullSink);
 
                 //Task.Delay(2000).Wait();
-                CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(200));
+                CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(250));
 
                 cam.ProcessAsync(cam.Camera.VideoPort, cts.Token).Wait();
                 return imgCaptureHandler.GetLastImage();
