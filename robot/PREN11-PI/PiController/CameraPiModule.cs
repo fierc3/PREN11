ee4a9e7ed9132685fc9a6d0c822b081d7f80be71
Mike <mpullen9@gmail.com>
2022-05-06 16:45:50 +0200

    Expand time during rect

--- robot/PREN11-PI/PiController/CameraPiModule.cs
+++ robot/PREN11-PI/PiController/CameraPiModule.cs
@@ -58,7 +58,7 @@ namespace Camera
                 cam.Camera.PreviewPort.ConnectTo(nullSink);
 
 
-                CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(750));
+                CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(1000));
 
                 cam.ProcessAsync(cam.Camera.VideoPort, cts.Token).Wait();
                 imgCaptureHandler.PostProcess();
