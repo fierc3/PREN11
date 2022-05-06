9fe6517303032cfbe8146cd7152c4f71b747b116
Mike <mpullen9@gmail.com>
2022-05-06 15:41:29 +0200

    Use video port for pictures

--- robot/PREN11-PI/PiController/CameraPiModule.cs
+++ robot/PREN11-PI/PiController/CameraPiModule.cs
@@ -9,13 +9,16 @@ using MMALSharp.Common;
 using MMALSharp.Handlers;
 using MMALSharp.Common.Utility;
 using MMALSharp.Native;
+using MMALSharp.Components;
+using MMALSharp.Ports;
+using System.Threading;
 
 namespace Camera
 {
     public class CameraPiModule : ICameraModule
     {
-
         MMALCamera cam;
+
         public void Init()
         {
             MMALCameraConfig.StillResolution = new Resolution(640, 480); // Set to 640 x 480. Default is 1280 x 720.
@@ -24,18 +27,34 @@ namespace Camera
             MMALCameraConfig.ISO = 400;
             MMALCameraConfig.StillBurstMode = true;
 
-            // Singleton initialized lazily. Reference once in your application.
-            cam = MMALCamera.Instance;
+             cam = MMALCamera.Instance;
         }
 
         public byte[] Read()
         {
             using (var imgCaptureHandler = new InMemoryCaptureHandler())
+            using (var splitter = new MMALSplitterComponent())
+            using (var imgEncoder = new MMALImageEncoder(continuousCapture: true))
+            using (var nullSink = new MMALNullSinkComponent())
             {
-                cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420).Wait();
+                cam.ConfigureCameraSettings();
+
+                var portConfig = new MMALPortConfig(MMALEncoding.JPEG, MMALEncoding.I420, quality: 90);
+
+                // Create our component pipeline.         
+                imgEncoder.ConfigureOutputPort(portConfig, imgCaptureHandler);
+
+                cam.Camera.VideoPort.ConnectTo(splitter);
+                splitter.Outputs[0].ConnectTo(imgEncoder);
+                cam.Camera.PreviewPort.ConnectTo(nullSink);
+
+
+                CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromSeconds(15));
+
+                // Process images for 15 seconds.        
+                cam.ProcessAsync(cam.Camera.VideoPort, cts.Token).Wait();
                 return imgCaptureHandler.WorkingData.ToArray();
             }
-            return null;
         }
 
         public void Release()
