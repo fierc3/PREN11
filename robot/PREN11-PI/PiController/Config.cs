5f9b99ef1999d79154a6d1b533fb5cfc99f47b9a
Mike <mpullen9@gmail.com>
2022-05-12 11:10:58 +0200

    Create Config.cs

---
+++ robot/PREN11-PI/PiController/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiController
+{
+    public class Config
+    {
+        public static string URL_PLANTID = "";
+        public static string URL_SERVERSOCKET = "https://tactile-rigging-333212.oa.r.appspot.com";
+        public static int SHUTTERSPEED = 20000;
+        public static int ISO = 100;
+        public static int CAMERA_ID_PI = 0;
+        public static int CAMERA_ID_USB = 1;
+    }
+}
