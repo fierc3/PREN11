9ea525b81dacc2dbf1efb43538e016763581163b
Mike <mpullen9@gmail.com>
2022-06-02 10:14:54 +0200

    Fix hidden search indicator in mobile view

--- backend/react/src/components/cards/InProgressCard.js
+++ backend/react/src/components/cards/InProgressCard.js
@@ -1,11 +1,12 @@
 import { CircularProgress, Typography } from "@mui/material";
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import useCheckMobileScreen from "../Utils";
 
 const InProgressCard = (props) => {
     return (
         <>
             <CircularProgress sx={{ color: "purple", opacity: 0.4, marginTop: "5vh", marginLeft: "2vw", width:"10vw" }} />
-            <Typography paddingLeft={"1vw"} gutterBottom variant="p" component="p">
+            <Typography paddingLeft={"1vw"} gutterBottom variant="p" component="p" paddingBottom={useCheckMobileScreen() ? '200px' : undefined}>
                 {props.text}
             </Typography>
         </>
